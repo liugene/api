9534bbdabcdf12c5011161acf3cd169838974a6e
LinkPHP <750688237@qq.com>
2017-09-23 16:40:04 +0800

    发帖

--- assets/models/main/ForumData.php
+++ assets/models/main/ForumData.php
@@ -30,16 +30,15 @@ class ForumData
             $data['title'] = $_POST['title'];
             $data['c_id'] = $_POST['type'];
             $data['u_id'] = 2;
-            $data['post_time'] = date('Y-m-d H:i:s',time());
+            $data['post_time'] = date('Y-m-d',time());
             $res['f_id'] = Db::table('lp_forum')
                 ->insert($data);
             var_dump($res);die;
             $res['content'] = $_POST['desc'];
             $res['u_id'] = 2;
             $res['post_time'] = date('Y-m-d H:i:s',time());
-            Db::table('lp_content')
+            return Db::table('lp_content')
                 ->insert($res);
-            return true;
         } catch (Exception $e) {
             return $e->getMessage();
         }
