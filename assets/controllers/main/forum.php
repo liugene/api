1a076a849c71da623b3a552f79e6f2e3d187cfd8
LinkPHP <750688237@qq.com>
2017-09-23 11:06:20 +0800

    发帖功能

---
+++ assets/controllers/main/forum.php
@@ -0,0 +1,38 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | LinkPHP [ Link All Thing ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 http://linkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liugene <liujun2199@vip.qq.com>
+// +----------------------------------------------------------------------
+// |               框架启动调度类
+// +----------------------------------------------------------------------
+namespace assets\controllers\main;
+use assets\models\main\ForumData;
+
+class Forum
+{
+    static public function getList()
+    {
+        header("Access-Control-Allow-Origin:*");
+        if(!empty(ForumData::getForumData())){
+            echo json_encode(['code' => 1, 'data' => ForumData::getForumData()], JSON_UNESCAPED_UNICODE);
+        } else {
+            echo json_encode(['code' => 2, 'data' => '暂时没有数据!'], JSON_UNESCAPED_UNICODE);
+        }
+    }
+
+    static public function push()
+    {
+        header("Access-Control-Allow-Origin:*");
+        if(ForumData::pushData()){
+            echo json_encode(['code' => 1, 'msg' => '发帖成功!'], JSON_UNESCAPED_UNICODE);
+        } else {
+            echo json_encode(['code' => 2, 'msg' => '发布失败!'], JSON_UNESCAPED_UNICODE);
+        }
+    }
+}
---
+++ assets/models/main/ForumData.php
@@ -0,0 +1,46 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | LinkPHP [ Link All Thing ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 http://linkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liugene <liujun2199@vip.qq.com>
+// +----------------------------------------------------------------------
+// |               框架启动调度类
+// +----------------------------------------------------------------------
+namespace assets\models\main;
+use util\db\Db;
+use linkphp\boot\Exception;
+
+class ForumData
+{
+    static public function getForumData()
+    {
+        return Db::table('lp_forum')
+            ->field('*')
+            ->select();
+    }
+
+    static public function pushData()
+    {
+        try{
+            $data['title'] = $_POST['title'];
+            $data['c_id'] = $_POST['type'];
+            $data['u_id'] = 2;
+            $data['post_time'] = date('Y-m-d H:i:s',time());
+            $res['f_id'] = Db::table('lp_forum')
+                ->insert($data);
+            $res['content'] = $_POST['desc'];
+            $res['u_id'] = 2;
+            $res['post_time'] = date('Y-m-d H:i:s',time());
+            Db::table('lp_content')
+                ->insert($res);
+            return true;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+}
