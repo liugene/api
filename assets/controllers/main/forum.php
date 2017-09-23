2681cabd981ec22ce35e49bc06f6414488f7fa9f
LinkPHP <750688237@qq.com>
2017-09-23 22:55:46 +0800

    发帖

--- assets/controllers/main/forum.php
+++ assets/controllers/main/forum.php
@@ -19,10 +19,6 @@ class Forum
     static public function getList()
     {
         header("Access-Control-Allow-Origin:*");
-        if(!$_POST){
-            echo json_encode(['code' => 2, 'msg' => '非法提交!'], JSON_UNESCAPED_UNICODE);
-            die;
-        }
         if(!empty(ForumData::getForumData())){
             echo json_encode(['code' => 1, 'data' => ForumData::getForumData()], JSON_UNESCAPED_UNICODE);
         } else {
@@ -47,10 +43,6 @@ class Forum
     static public function getItems()
     {
         header("Access-Control-Allow-Origin:*");
-        if(!$_POST){
-            echo json_encode(['code' => 2, 'msg' => '非法提交!'], JSON_UNESCAPED_UNICODE);
-            die;
-        }
         if(!empty(ForumData::getItemsData())){
             echo json_encode(['code' => 1, 'data' => ForumData::getItemsData()], JSON_UNESCAPED_UNICODE);
         } else {
