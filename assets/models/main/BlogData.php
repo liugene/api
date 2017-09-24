6f8409769dd71f9e1b73c4d27c67da6af98f7c73
LinkPHP <750688237@qq.com>
2017-09-24 21:57:12 +0800

    更新

---
+++ assets/models/main/BlogData.php
@@ -0,0 +1,26 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | LinkPHP [ Link All Thing ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 http://linkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liugene <liujun2199@vip.qq.com>
+// +----------------------------------------------------------------------
+// |               框架启动调度类
+// +----------------------------------------------------------------------
+namespace assets\models\main;
+use util\db\Db;
+
+class BlogData
+{
+    static public function getListData()
+    {
+        return Db::table('lp_blog')
+            ->field('*')
+            ->where('where is_on = 0')
+            ->select();
+    }
+}
