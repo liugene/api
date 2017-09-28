3e5d974de08c771b0877cd42848037d4f3e35fe0
liugene <jun.liu@jrexpo.com>
2017-09-28 17:10:45 +0800

    判断是否登入修复

--- assets/controllers/main/Auth.php
+++ assets/controllers/main/Auth.php
@@ -18,7 +18,7 @@ class Auth
 {
     static public function isLogin()
     {
-        if(isset($_SERVER['HTTP_AUTHORIZATION'])){
+        if(isset($_SERVER['HTTP_AUTHORIZATION']) && !empty($_SERVER['HTTP_AUTHORIZATION'])){
             $data = Jwt::parse($_SERVER['HTTP_AUTHORIZATION']);
             if(Jwt::verify($data,'123')){
                 echo json_encode(['code' => 1, 'data' => Jwt::arrays($data['payload'])], JSON_UNESCAPED_UNICODE);
