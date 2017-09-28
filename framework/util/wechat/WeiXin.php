ff4cc4fd085045c34e5b8ca2eca36f0048f65acf
LinkPHP <750688237@qq.com>
2017-09-28 22:36:56 +0800

    微信公众号验证

--- framework/util/wechat/WeiXin.php
+++ framework/util/wechat/WeiXin.php
@@ -26,8 +26,8 @@ class WeiXin{
                $signature = $_GET['signature'];
                //形成数组。然后按字典序排序
                $array = [$nonce,$timestamp,$token];
-               $str = implode($array);
                sort($str, SORT_STRING);
+               $str = implode($array);
                //拼接成字符串，使用sha1加密，然后与signatrue进行校验
                $str = sha1($str);
                if($str === $signature){
